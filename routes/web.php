301ffdceeee2536b37a72b3d65ca9dde845ac9c6
yashg953 <yashg953@gmail.com>
2024-01-13 20:00:24 +0530

    first view created

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/documentation', function () {
+    return view('documentation');
+});
+Route::get('/hello', function () {
+    return view('hello');
 });
